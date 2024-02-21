b1dbfb859ddf08322632d7ad7b7f6dfd19239bcd
jrp88-ua <78471953+jrp88-ua@users.noreply.github.com>
2024-02-21 18:42:41 +0100

    added i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
 </script>
 
-<slot />
+<ParaglideJS {i18n}>
+	<slot />
+</ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { paraglide } from '@inlang/paraglide-js-adapter-sveltekit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/paraglide'
+		}),
+		sveltekit(),
+		purgeCss()
+	]
 });
