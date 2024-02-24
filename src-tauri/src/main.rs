d483594b47313969dcd2d7011f30b0443495ef9a
jrp88-ua <78471953+jrp88-ua@users.noreply.github.com>
2024-02-24 13:35:20 +0100

    save

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello {} from Rust!", name)
-}
+mod storage;
+
+use log::LevelFilter;
+use tauri_plugin_log::LogTarget;
+
+#[cfg(debug_assertions)]
+const LOG_TARGETS: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::Webview];
+
+#[cfg(not(debug_assertions))]
+const LOG_TARGETS: [LogTarget; 2] = [LogTarget::Stdout, LogTarget::LogDir];
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            storage::database::open_db_file_with_password
+        ])
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets(LOG_TARGETS)
+                .with_colors(tauri_plugin_log::fern::colors::ColoredLevelConfig::default())
+                .level_for("tauri", log::LevelFilter::Info)
+                .level(LevelFilter::Debug)
+                .build(),
+        )
+        .manage(storage::database::DatabaseState::default())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/routes/participants/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api";
+
+    let p = invoke("open_db_file_with_password", {
+        password: "juan"
+    });
+</script>
+
+{#await p}
+    Loading...
+{:then v} 
+    {@html v}
+{:catch e}
+    {@html e}
+{/await}
