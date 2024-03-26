145f312e07d181e3279d04cca91a03f7b90fbe69
jrp88-ua <78471953+jrp88-ua@users.noreply.github.com>
2024-03-26 11:57:42 +0100

    build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 import { storePopup } from '@skeletonlabs/skeleton';
 
-//export const prerender = true;
+export const prerender = true;
 export const ssr = false;
 //export const csr = true;
 
