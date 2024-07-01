3aa083608b68dd20fb8fa71a5490f7b74ad9c74d
jrp88-ua <78471953+jrp88-ua@users.noreply.github.com>
2024-07-01 19:22:48 +0200

    depend update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-extern crate argon2;
+#![warn(unused_extern_crates)]
 
 mod event;
 mod ipc;
